19203a9a8d5df35baa7b9c05f2b17e6be1ce8d0e
Balantaim <martin.ned.atanasov@gmail.com>
2023-04-30 21:16:36 +0300

    some UI updates, added error message

--- src/main/java/com/martinatanasov/springexample/config/WebSecurityConfig.java
+++ src/main/java/com/martinatanasov/springexample/config/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.martinatanasov.springexample.configuration;
+package com.martinatanasov.springexample.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +9,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -22,11 +23,30 @@ public class WebSecurityConfig {
                 )
                 .formLogin((form) -> form
                         .loginPage("/login")
+                        //login with email
+                        //.usernameParameter("email")
                         .permitAll()
                 )
                 .logout((logout) -> logout.permitAll());
-
         return http.build();
+
+        /*
+        http.csrf().disable()
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/", "/login","/register", "/js/**", "/css/**", "/images/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(from -> from
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/userControlPanel")
+                        .loginProcessingUrl("/login")
+                        .failureUrl("/index")
+                        .permitAll()
+                ).logout(logout -> logout
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout")).permitAll()
+                );
+        return http.build();
+         */
     }
 
     @Bean
--- src/main/java/com/martinatanasov/springexample/controller/Controller.java
+++ src/main/java/com/martinatanasov/springexample/controller/Controller.java
@@ -1,17 +1,12 @@
 package com.martinatanasov.springexample.controller;
 
 import com.martinatanasov.springexample.model.User;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Objects;
-
-@Controller
-public class IndexController {
+@org.springframework.stereotype.Controller
+public class Controller {
 
     @GetMapping("/")
     public String index(){
